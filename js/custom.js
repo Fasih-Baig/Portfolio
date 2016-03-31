fdf7344f0def7b4f4270394319c57bf468c4e5fe
fasih baig <fasihbaig93@gmail.com>
2016-03-31 12:49:21 +0500

    Added my work well

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
   $(".navbar-brand").addClass("animated zoomIn");
   $(".underscore").addClass("animated flash");
+  $(".btn-primary").addClass("animated bounce");
+
+  $("[rel='tooltip']").tooltip();    
+ 
+    $('.thumb').hover(
+        function(){
+            $(this).find('.caption').slideDown(250); //.slideDown(250)
+        },
+        function(){
+            $(this).find('.caption').slideUp(250); //.slideUp(250)
+        }
+    ); 
 });
+
+
